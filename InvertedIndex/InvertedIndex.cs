a6966cd67b18419371f3c91d1e4cb4c4b33beeaf
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-11-27 18:58:05 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ InvertedIndex/InvertedIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BerkeleyDB;
+
+namespace InvertedIndex
+{
+    public class InvertedIndex
+    {
+        private HashDatabase hashDatabase;
+
+        public InvertedIndex()
+        {
+            hashDatabase = HashDatabase.Open("inverted_index.db", new HashDatabaseConfig());
+            Console.WriteLine("DB created");
+        }
+    }
+}
--- InvertedIndex/Program.cs
+++ InvertedIndex/Program.cs
@@ -13,7 +13,7 @@ namespace InvertedIndex
     {
         public static void Main(string[] args)
         {
-            var a = BerkeleyDB.HashDatabase.Open("inverted_index.db", new BerkeleyDB.HashDatabaseConfig());
+            InvertedIndex a = new InvertedIndex();
             //CreateHostBuilder(args).Build().Run();
         }
 
